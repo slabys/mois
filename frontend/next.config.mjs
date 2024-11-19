60ac017d1fea757a42f09c395df905299239a546
slabys <simi.sla@seznam.cz>
2024-11-19 23:43:06 +0100

    Allow all origins, fix .env.example, fe api

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,7 +7,14 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks", "@mantine/dates"],
   },
-
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:4000/:path*", // Proxy to the backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -25,37 +25,54 @@ const LoginForm = ({}: LoginFormProps) => {
   });
 
   const loginUser = async (values: LoginFormType) => {
-    // console.log(values);
-    const res = await apiFetch("https://cat-fact.herokuapp.com/facts", "GET");
-    return res;
-    // console.log(data, status, statusText);
+    const res = await apiFetch("/auth/login", {
+      method: "POST",
+      data: {
+        email: "info@slabys.cz",
+        password: "password",
+      },
+      withCredentials: true,
+    });
+    console.log(res);
+  };
+
+  const testMe = async () => {
+    const res = await apiFetch("/users", {
+      method: "GET",
+    });
+    console.log(res);
   };
 
   return (
-    <form
-      onSubmit={form.onSubmit((values) => {
-        loginUser(values).then((data) => console.log(data));
-      })}
-    >
-      <Flex direction="column" gap={12}>
-        <TextInput label="Username" {...form.getInputProps("username")} />
-        <TextInput label="Password" type="password" {...form.getInputProps("password")} />
-      </Flex>
-      <Flex direction="column" gap={16} mt={16}>
-        {/*TODO loading*/}
-        <Button loading={false} type="submit">
-          Log In
-        </Button>
-        {/* LOGIN ERROR */}
-        <Text c="bfiRed.6">Something went wrong! Please try again.</Text>
-        <Stack gap={8}>
-          <Text c="bfiPositive.8" fw={700}>
-            Password reset has been requested.
-          </Text>
-          <Text c="bfiPositive.8">Email with instructions has been sent to the associated email address. </Text>
-        </Stack>
-      </Flex>
-    </form>
+    <>
+      <form
+        onSubmit={form.onSubmit((values) => {
+          loginUser(values);
+        })}
+      >
+        <Flex direction="column" gap={12}>
+          <TextInput label="Username" {...form.getInputProps("username")} />
+          <TextInput label="Password" type="password" {...form.getInputProps("password")} />
+        </Flex>
+        <Flex direction="column" gap={16} mt={16}>
+          {/*TODO loading*/}
+          <Button loading={false} type="submit">
+            Log In
+          </Button>
+          {/* LOGIN ERROR */}
+          <Text c="bfiRed.6">Something went wrong! Please try again.</Text>
+          <Stack gap={8}>
+            <Text c="bfiPositive.8" fw={700}>
+              Password reset has been requested.
+            </Text>
+            <Text c="bfiPositive.8">Email with instructions has been sent to the associated email address. </Text>
+          </Stack>
+        </Flex>
+      </form>
+      <Button loading={false} onClick={() => testMe()}>
+        Funguj
+      </Button>
+    </>
   );
 };
 
