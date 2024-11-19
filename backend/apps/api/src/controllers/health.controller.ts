8f01077e42048b1d90f0823fd2b713f6ec2bab36
Petr Vavřínek <vavrinekpetr155@gmail.com>
2024-11-19 19:38:39 +0100

    feat: backend_app1 docker compose healthcheck

---
+++ backend/apps/api/src/controllers/health.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { ApiExcludeController } from "@nestjs/swagger";
+
+@ApiExcludeController()
+@Controller("health")
+export class HealthController {
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  health() {}
+}
--- backend/apps/api/src/controllers/index.ts
+++ backend/apps/api/src/controllers/index.ts
@@ -1,7 +1,9 @@
 export { AuthController } from "./auth.controller";
 export { EventsController } from "./events.controller";
+export { HealthController } from "./health.controller";
 export { OrganizationMembersController } from "./organization-members.controller";
 export { OrganizationsController } from "./organizations.controller";
 export { PhotoController } from "./photo.controller";
 export { UniversitiesController } from "./universities.controller";
 export { UsersController } from "./users.controller";
+
