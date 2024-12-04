2c05ca144d48208b41efcb6f90f25431c30ee836
Petr Mlejnek <mlejnpe1@uhk.cz>
2024-12-04 08:34:26 +0100

    added CreateEvent form Modal

--- frontend/src/app/(authorized)/manage-events/page.tsx
+++ frontend/src/app/(authorized)/manage-events/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUpcomingEvents } from "@/utils/api";
+import AddEventModal from "@components/AddEventModal/AddEventModal";
 import {
   Button,
   Container,
@@ -15,9 +16,14 @@ import {
   Title,
 } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
-import Link from "next/link";
+import { useState } from "react";
 
 const ManageEventsPage = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   const data = useUpcomingEvents();
   console.log("data", data.data);
 
@@ -60,9 +66,10 @@ const ManageEventsPage = () => {
       <Stack>
         <Flex justify="space-between" align="center" w="100%">
           <Title>Manage Events</Title>
-          <Button component={Link} href="" leftSection={<IconPlus />}>
+          <Button onClick={openModal} leftSection={<IconPlus />}>
             Add Event
           </Button>
+          <AddEventModal isOpen={modalOpen} onClose={closeModal} />
         </Flex>
         <Table
           withTableBorder
