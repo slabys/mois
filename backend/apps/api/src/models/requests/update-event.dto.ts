a5bbcc5d8bc77b0b580a960c107416bf2af96250
Petr Vavřínek <vavrinekpetr155@gmail.com>
2024-12-07 17:55:40 +0100

    feat: restrict update event registrationForm

--- backend/apps/api/src/models/requests/update-event.dto.ts
+++ backend/apps/api/src/models/requests/update-event.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from "@nestjs/swagger";
+import { OmitType, PartialType } from "@nestjs/swagger";
 import { CreateEvent } from "./create-event.dto";
 
-export class UpdateEvent extends PartialType(CreateEvent) {}
+export class UpdateEvent extends OmitType(PartialType(CreateEvent), [
+  "registrationForm",
+]) {}
