abe4d4b296393344309a23964fdf0ca553cca623
slabys <simi.sla@seznam.cz>
2024-12-08 22:24:30 +0100

    Check if enum types exists

--- backend/database/migrations/1731954655025-create_role_entity.ts
+++ backend/database/migrations/1731954655025-create_role_entity.ts
@@ -4,7 +4,17 @@ export class CreateRoleEntity1731954655025 implements MigrationInterface {
   name = "CreateRoleEntity1731954655025";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TYPE IF NOT EXIST "public"."role_permissions_enum" AS ENUM('create.event')`);
+    // Check if the enum type exists
+    const typeExists = await queryRunner.query(`
+        SELECT EXISTS (SELECT 1
+                       FROM pg_type
+                       WHERE typname = 'role_permissions_enum')
+    `);
+
+    if (!typeExists[0].exists) {
+      await queryRunner.query(`CREATE TYPE "public"."role_permissions_enum" AS ENUM('create.event')`);
+    }
+
     await queryRunner.query(`CREATE TABLE "role"
                              (
                                  "id"          character varying NOT NULL,
@@ -16,7 +26,7 @@ export class CreateRoleEntity1731954655025 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "role"`);
-    await queryRunner.query(`DROP TYPE IF NOT EXIST "public"."role_permissions_enum"`);
+    await queryRunner.query(`DROP TYPE IF EXISTS "public"."role_permissions_enum"`);
   }
 
 }
--- backend/database/migrations/1733576785768-update_user_add_gender_column.ts
+++ backend/database/migrations/1733576785768-update_user_add_gender_column.ts
@@ -1,16 +1,27 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UpdateUserAddGenderColumn1733576785768 implements MigrationInterface {
-    name = 'UpdateUserAddGenderColumn1733576785768'
+  name = "UpdateUserAddGenderColumn1733576785768";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."user_gender_enum" AS ENUM('male', 'female', 'non-binary', 'prefer-not-to-say')`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "gender" "public"."user_gender_enum" NOT NULL DEFAULT 'prefer-not-to-say'`);
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Check if the enum type exists
+    const typeExists = await queryRunner.query(`
+        SELECT EXISTS (SELECT 1
+                       FROM pg_type
+                       WHERE typname = 'user_gender_enum')
+    `);
+
+    if (!typeExists[0].exists) {
+      await queryRunner.query(`CREATE TYPE "public"."user_gender_enum" AS ENUM('male', 'female', 'non-binary', 'prefer-not-to-say')`);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "gender"`);
-        await queryRunner.query(`DROP TYPE "public"."user_gender_enum"`);
-    }
+    await queryRunner.query(`ALTER TABLE "user"
+        ADD "gender" "public"."user_gender_enum" NOT NULL DEFAULT 'prefer-not-to-say'`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "gender"`);
+    await queryRunner.query(`DROP TYPE IF EXISTS "public"."user_gender_enum"`);
+  }
 
 }
