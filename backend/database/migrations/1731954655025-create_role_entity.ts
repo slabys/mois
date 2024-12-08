9f57b58eb9d881b3bea55b71621832ccec5c7122
slabys <simi.sla@seznam.cz>
2024-12-08 22:09:56 +0100

    Migration type if not exists

--- backend/database/migrations/1731954655025-create_role_entity.ts
+++ backend/database/migrations/1731954655025-create_role_entity.ts
@@ -1,16 +1,22 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateRoleEntity1731954655025 implements MigrationInterface {
-    name = 'CreateRoleEntity1731954655025'
+  name = "CreateRoleEntity1731954655025";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."role_permissions_enum" AS ENUM('create.event')`);
-        await queryRunner.query(`CREATE TABLE "role" ("id" character varying NOT NULL, "name" character varying NOT NULL, "permissions" "public"."role_permissions_enum" array NOT NULL, CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TYPE IF NOT EXIST "public"."role_permissions_enum" AS ENUM('create.event')`);
+    await queryRunner.query(`CREATE TABLE "role"
+                             (
+                                 "id"          character varying NOT NULL,
+                                 "name"        character varying NOT NULL,
+                                 "permissions" "public"."role_permissions_enum" array NOT NULL,
+                                 CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id")
+                             )`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "role"`);
-        await queryRunner.query(`DROP TYPE "public"."role_permissions_enum"`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "role"`);
+    await queryRunner.query(`DROP TYPE IF NOT EXIST "public"."role_permissions_enum"`);
+  }
 
 }
