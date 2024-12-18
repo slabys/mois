2ba33a1f745c18c6684d42910ae48cc20218ba3d
slabys <simi.sla@seznam.cz>
2024-12-18 11:32:25 +0100

    catch event fetch pages

--- frontend/src/app/(authorized)/event/manage/[id]/page.tsx
+++ frontend/src/app/(authorized)/event/manage/[id]/page.tsx
@@ -23,9 +23,13 @@ export const revalidate = 60;
 
 const UseFetchEventApplications = async () => {
   const queryKey = getGetEventsQueryKey();
-  const events: EventSimple[] = await fetch(`${process.env.NEXT_PUBLIC_APP1_URL}${queryKey[0]}`).then((res) =>
-    res.json(),
-  );
+  const events: EventSimple[] = await fetch(`${process.env.NEXT_PUBLIC_APP1_URL}${queryKey[0]}`)
+    .then((res) => res.json())
+    .catch((e) => console.error(e));
+
+  if (!events) {
+    return [];
+  }
 
   return events.map((event) => {
     return {
