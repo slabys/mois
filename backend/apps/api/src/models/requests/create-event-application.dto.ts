dfb5c650221e48e2c35831e74ef8b1556b17ce80
Petr Vavřínek <vavrinekpetr155@gmail.com>
2024-12-18 11:08:30 +0100

    fix: create event application dto validation

--- backend/apps/api/src/models/requests/create-event-application.dto.ts
+++ backend/apps/api/src/models/requests/create-event-application.dto.ts
@@ -1,5 +1,14 @@
 import { Type } from "class-transformer";
-import { Allow, IsNotEmptyObject, IsNumber, IsObject, IsOptional, IsString } from "class-validator";
+import {
+  Allow,
+  IsEnum,
+  IsIn,
+  IsNotEmptyObject,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from "class-validator";
 import { CreateAddress } from "./create-address.dto";
 import { ApiExtraModels, ApiProperty, refs } from "@nestjs/swagger";
 
@@ -7,7 +16,7 @@ export class CreateEventApplicationExistingOrganization {
   /**
    * Organization ID
    */
-  @Allow()
+  @IsString()
   id: string;
 
   @ApiProperty({
@@ -19,10 +28,10 @@ export class CreateEventApplicationExistingOrganization {
 }
 
 export class CreateEventApplicationCustomOrganization {
-  @Allow()
+  @IsString()
   name: string;
 
-  @Allow()
+  @IsString()
   country: string;
 
   @ApiProperty({
