87ab04768ec74299d614ccfce14023408717071d
slabys <simi.sla@seznam.cz>
2025-02-05 20:22:58 +0100

    FE - Config set to api

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
       },
     },
     input: {
-      target: "http://localhost:4000/docs-json",
-      // target: "https://api.slabys.cz/docs-json",
+      // target: "http://localhost:4000/docs-json",
+      target: "https://api.slabys.cz/docs-json",
     },
     hooks: {
       afterAllFilesWrite: ["yarn prettier --write ./src/utils/api.ts"],
