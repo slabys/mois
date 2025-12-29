c1024983486ee942dfecefbaf5a2e36cbf19cc79
slabys <simi.sla@seznam.cz>
2025-12-29 20:08:46 +0100

    Sortable link tree

--- frontend/src/components/events/modals/EventEditModal.tsx
+++ frontend/src/components/events/modals/EventEditModal.tsx
@@ -1,23 +1,19 @@
 "use client";
 
 import { useUpdateEvent } from "@/utils/api";
-import { EventDetail, type UpdateEvent, UpdateEventLinkPartial } from "@/utils/api.schemas";
+import { EventDetail, type UpdateEvent } from "@/utils/api.schemas";
 import Modal from "@components/Modal/Modal";
 import RichTextEditor from "@components/Richtext/RichTextEditor";
+import LinkTree, { LinkItem } from "@components/events/LinkTree";
 import DateInput from "@components/primitives/DateInput";
-import { ActionIcon, Button, Fieldset, Flex, Group, NumberInput, SimpleGrid, Switch, TextInput } from "@mantine/core";
+import { Button, Fieldset, Flex, Group, NumberInput, SimpleGrid, Switch, TextInput } from "@mantine/core";
 import { DateTimePicker } from "@mantine/dates";
 import { Form, useForm } from "@mantine/form";
-import { IconLinkPlus, IconTrash } from "@tabler/icons-react";
 import dayjs from "dayjs";
 import React from "react";
 
-type ExtendedEventLinkType = UpdateEventLinkPartial & {
-  customId?: string;
-};
-
 type FormUpdateEvent = Omit<UpdateEvent, "links"> & {
-  links: ExtendedEventLinkType[];
+  links: LinkItem[];
 };
 
 interface EventEditModalProps {
@@ -36,7 +32,7 @@ const EventEditModal = ({ eventDetail, isOpened, close, handleSuccess = () => {}
       capacity: eventDetail.capacity,
       shortDescription: eventDetail.shortDescription,
       longDescription: eventDetail.longDescription,
-      links: eventDetail.links,
+      links: eventDetail.links.map((link) => ({ ...link, customId: crypto.randomUUID() })),
       since: eventDetail.since,
       until: eventDetail.until,
       codeOfConductLink: eventDetail.codeOfConductLink,
@@ -53,7 +49,10 @@ const EventEditModal = ({ eventDetail, isOpened, close, handleSuccess = () => {}
   const eventUpdateMutation = useUpdateEvent({
     mutation: {
       onSuccess: (data) => {
-        form.setInitialValues(data);
+        form.setInitialValues({
+          ...data,
+          links: data.links.map(({ name, link, id }) => ({ id, name, link, customId: crypto.randomUUID() })),
+        });
         form.reset();
         handleSuccess();
         close();
@@ -61,36 +60,14 @@ const EventEditModal = ({ eventDetail, isOpened, close, handleSuccess = () => {}
     },
   });
 
-  const handleEventUpdate = (values: UpdateEvent) => {
+  const handleEventUpdate = (values: FormUpdateEvent) => {
     eventUpdateMutation.mutate({
       eventId: eventDetail.id,
-      data: { ...values, links: values.links.map(({ name, link }) => ({ id: null, name, link })) },
+      data: { ...values, links: values.links.map(({ name, link, id }) => ({ id: id ?? null, name, link })) },
     });
   };
 
   const isTouchedDirty = form.isTouched() && form.isDirty();
-
-  const handleRemoveLink = (link: ExtendedEventLinkType) => {
-    if (link.id !== null) {
-      form.setFieldValue("links", (prevValue) => prevValue?.filter((value) => value.id !== link.id));
-      return;
-    }
-    form.setFieldValue("links", (prevValue) => prevValue?.filter((value) => value.customId !== link.customId));
-  };
-
-  const handleAddNewLink = () => {
-    form.setFieldValue("links", (prevValue) => {
-      return [
-        ...(prevValue ?? []),
-        {
-          customId: crypto.randomUUID(),
-          id: null,
-          name: "",
-          link: "",
-        },
-      ];
-    });
-  };
 
   const handleClose = () => {
     form.reset();
@@ -147,45 +124,7 @@ const EventEditModal = ({ eventDetail, isOpened, close, handleSuccess = () => {}
           <RichTextEditor label="Long Description" {...form.getInputProps("longDescription")} />
         </Flex>
         <Fieldset legend="Link tree" variant="filled" p={16}>
-          <Flex direction="column" gap={8} w="100%">
-            {form.values.links?.map((value, index) => {
-              return (
-                <Flex key={`link-tree-create-${index}-${value.customId}`} w="100%" gap={16}>
-                  <Flex direction={{ base: "column", md: "row" }} w="100%" gap={{ base: 8, md: 16 }}>
-                    <TextInput
-                      label="Name"
-                      placeholder="ERS link"
-                      w="100%"
-                      value={value.name}
-                      onChange={(event) => {
-                        event.preventDefault();
-                        form.setFieldValue(`links.${index}.name`, event.currentTarget.value);
-                      }}
-                    />
-                    <TextInput
-                      label="URL"
-                      placeholder="https://www.ers.cz/"
-                      w="100%"
-                      value={value.link}
-                      onChange={(event) => {
-                        form.setFieldValue(`links.${index}.link`, event.currentTarget.value);
-                      }}
-                    />
-                  </Flex>
-                  <Flex w="fit-content" justify="center" align="center">
-                    <ActionIcon onClick={() => handleRemoveLink(value)} variant="light" color="red" size="lg">
-                      <IconTrash />
-                    </ActionIcon>
-                  </Flex>
-                </Flex>
-              );
-            })}
-          </Flex>
-          <Flex justify="center" align="center" mt={16}>
-            <Button leftSection={<IconLinkPlus />} onClick={handleAddNewLink} variant="light">
-              Add new link
-            </Button>
-          </Flex>
+          <LinkTree links={form.values.links ?? []} onChange={(links) => form.setFieldValue("links", links)} />
         </Fieldset>
         <Group justify="center" mt="lg">
           <Button type="submit" disabled={!isTouchedDirty} loading={eventUpdateMutation.isPending}>
